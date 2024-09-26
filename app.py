23e543162ec0d0c74f23f08250ba416210daabde
Devansh Pratap Singh <devanshprataps6@gmail.com>
2024-09-26 12:04:29 +0530

    This commit is for web application

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template,redirect,url_for,jsonify
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
 
-app=Flask(__name__)
-# Load the model
-scalar=pickle.load(open('scalar.pkl','rb'))
-model=pickle.load(open('rainsvcmodel.pkl','rb'))
+app = Flask(__name__)
 
-@app.route("/",methods=["GET"])
+# Load the model and scaler
+scalar = pickle.load(open('scalar.pkl', 'rb'))
+model = pickle.load(open('rainsvcmodel.pkl', 'rb'))
+
+@app.route("/", methods=["GET"])
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data=request.json['data']
-    new_data=np.array(list(data.values())).reshape(1,-1)
-    scaled_data=scalar.transform(new_data)
-    output=model.predict(scaled_data)
-    print(output[0])
-    return jsonify(output[0])
-
+    data = request.get_json(force=True)
+    new_data = np.array(list(data.values())).reshape(1, -1)
+    scaled_data = scalar.transform(new_data)
+    output = model.predict(scaled_data)
+    temp=int(output[0])
+    res=""
+    if temp==1:
+        res="Rainfall will occur."
+    else:
+       res="Rainfall will not occur." 
+    return jsonify(res)   
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
