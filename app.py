f3085e2bb22b3a44c621cae4037c334a3ef9321b
Devansh Pratap Singh <devanshprataps6@gmail.com>
2024-09-26 09:07:01 +0530

    Updated app.py file like importing pickle files etc.

--- app.py
+++ app.py
@@ -4,8 +4,22 @@ import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
+# Load the model
+scalar=pickle.load(open('scalar.pkl','rb'))
+model=pickle.load(open('rainsvcmodel.pkl','rb'))
 
 @app.route("/",methods=["GET"])
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    new_data=np.array(list(data.values())).reshape(1,-1)
+    scaled_data=scalar.transform(new_data)
+    output=model.predict(scaled_data)
+    print(output[0])
+    return jsonify(output[0])
 
 
 if __name__ == "__main__":
